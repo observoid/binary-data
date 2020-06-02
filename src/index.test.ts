78e552cd9f07073308f3cbd501e4b2f02e5387dc
Duncan Cross <duncan@azuki.io>
2020-06-02 15:56:37 +0100

    Add alignBytes

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,13 +1,40 @@
 
 import { TestHarness } from 'zora';
-import {  } from './index';
+import { alignBytes, ArrayBufferViewFactory } from '../lib/index';
+import { of } from 'rxjs';
+import { toArray } from 'rxjs/operators';
 
 export default (t: TestHarness) => {
 
-  /*
-  t.test('name', async t => {
-    // ...
+  t.test('alignBytes', async t => {
+
+    const makeByteArray: ArrayBufferViewFactory<Uint8Array> = (buffer: ArrayBuffer, byteOffset: number, byteLength: number) => new Uint8Array(buffer, byteOffset, byteLength);
+
+    t.test('one-byte alignment', async t => {
+      const result = await of(new Uint8Array([1, 2, 3, 4, 5]))
+        .pipe(
+          alignBytes(makeByteArray, 1),
+          toArray()
+        )
+        .toPromise();
+      t.eq(result.length, 1);
+      t.eq(Array.prototype.slice.apply(result[0]), [1, 2, 3, 4, 5]);
+    });
+
+    t.test('multi-byte alignment', async t => {
+      const result = await of(...[ [1, 2, 3, 4, 5, 6], [7], [8], [9], [10, 11, 12, 13] ].map(v => new Uint8Array(v)))
+        .pipe(
+          alignBytes(makeByteArray, 3),
+          toArray()
+        )
+        .toPromise();
+      t.eq(result.length, 4);
+      t.eq(Array.prototype.slice.apply(result[0]), [1, 2, 3, 4, 5, 6]);
+      t.eq(Array.prototype.slice.apply(result[1]), [7, 8, 9]);
+      t.eq(Array.prototype.slice.apply(result[2]), [10, 11, 12]);
+      t.eq(Array.prototype.slice.apply(result[3]), [13, 0, 0]);
+    });
+
   });
-  */
 
 }
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,49 @@
 
-import { Observable } from 'rxjs';
+import { OperatorFunction, Observable } from 'rxjs'
+
+export type ArrayBufferViewFactory<TView> = (arrayBuffer: ArrayBuffer, byteOffset: number, byteLength: number) => TView;
+
+export function alignBytes<T extends {buffer:ArrayBuffer, byteOffset:number, byteLength:number}>(createView: ArrayBufferViewFactory<T>, alignByteLength: number): OperatorFunction<T, T> {
+  if (alignByteLength < 2) return input => input;
+  return input => new Observable(subscriber => {
+    let prefix: Uint8Array | null = null;
+    let prefixOffset = 0;
+    return input.subscribe(
+      chunk => {
+        if (prefixOffset !== 0) {
+          const copy = new Uint8Array(chunk.buffer, chunk.byteOffset, Math.min(alignByteLength - prefixOffset, chunk.byteLength));
+          prefix!.set(copy, prefixOffset);
+          prefixOffset += copy.length;
+          if (prefixOffset < alignByteLength) {
+            return;
+          }
+          subscriber.next(createView(prefix!.buffer, prefix!.byteOffset, prefix!.byteLength));
+          prefix = null;
+          chunk = createView(chunk.buffer, chunk.byteOffset + copy.length, chunk.byteLength - copy.length);
+        }
+        prefixOffset = chunk.byteLength % alignByteLength;
+        if (prefixOffset === 0) {
+          if (chunk.byteLength !== 0) {
+            subscriber.next(chunk);
+          }
+        }
+        else {
+          const rest = chunk.byteLength - prefixOffset;
+          if (rest !== 0) {
+            subscriber.next(createView(chunk.buffer, chunk.byteOffset, rest));
+          }
+          prefix = new Uint8Array(alignByteLength);
+          prefix.set(new Uint8Array(chunk.buffer, chunk.byteOffset + rest));
+        }
+      },
+      e => subscriber.error(e),
+      () => {
+        if (prefix) {
+          subscriber.next(createView(prefix.buffer, prefix.byteOffset, prefix.byteLength));
+          prefix = null;
+        }
+        subscriber.complete();
+      }
+    );
+  });
+}
